edeaa9bafa87f0e54b9905533c8d3ecdb7cb7c25
jiyange <program.ji.yang@gmail.com>
2016-02-15 22:13:05 -0700

    Updated basic models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from . import db
 
 
+class Permission:
+    pass
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +22,56 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Colunm(db.String(64))
+    username = db.Column(db.String(64), unique=True, index=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    
+class Comment(db.model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    content = title = db.Colunm(db.String(500))
+
+
+class Image(db.model):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    file = 0
+
+    
+class Privacy:
+    pass
+
+
+class Friend(db.model):
+    a_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    b_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def integrityCheck(self, a_id, b_id):
+        return a_id == b_id
+    
+class Follow(db.model):
+    requester_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    requestee_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def integrityCheck(self, requester_id, requestee_id):
+        return requester_id == requestee_id
+    
+
+class Node:
+    pass
+
+class APIRequest:
+    pass
+
+class NodeAPI:
+    pass
+
+
