58f750803daba760e496474db6d6263d7a8a1b1e
jiyange <program.ji.yang@gmail.com>
2016-03-06 16:07:18 -0700

    post model and db setup

--- fbook/main/forms.py
+++ fbook/main/forms.py
@@ -5,10 +5,6 @@ from flask.ext.pagedown.fields import PageDownField
 
 
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
-
 class PostForm(Form):
     body = PageDownField('What is on your mind?', validators=[Required()])
     submit = SubmitField("Post")
