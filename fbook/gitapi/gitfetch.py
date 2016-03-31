7c9b58c86e80c7a68340debca53f45ad4f0cac1f
jiyange <program.ji.yang@gmail.com>
2016-03-31 03:30:36 -0600

    Return a list contains all events for selected user

--- fbook/gitapi/gitfetch.py
+++ fbook/gitapi/gitfetch.py
@@ -4,6 +4,7 @@ from urllib2 import urlopen, URLError, HTTPError # In python 3 change to urllib.
 import json
 
 def fetch_events(user_id):
+    returned_list = []
     try:
         html = urlopen('https://api.github.com/users/' + user_id + '/events')
     # In python 3, add .decode('UTF-8') after read()
@@ -13,4 +14,14 @@ def fetch_events(user_id):
         json_str = (html.read())
         json_dict = json.loads(json_str)
         for event in json_dict:
-            print event
+            result = {'id': event['id'],
+                      'type': event['type'],
+                      'actor': user_id,
+                      'repo': event['repo']['name'],
+                      'repo_url': event['repo']['url'],
+                      'public': event['public'],
+                      'created_at': event['created_at']}
+            print result
+            returned_list.append(result)
+    if len(returned_list) is not 0:
+        return returned_list
