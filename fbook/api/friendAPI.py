7bbc8110665872f293a8348dd38ec0f26e401233
Cody Ingram <codyingram@uws112433usr.uws.ualberta.ca>
2016-04-02 15:40:30 -0600

    looking for api parsing error

--- fbook/api/friendAPI.py
+++ fbook/api/friendAPI.py
@@ -123,6 +123,9 @@ class friend_request(Resource):
 		args = parser.parse_args();
 		author = args['author']
 		friend = args['friend']
+		print args
+		print author
+		print friend
 		# try to add author to remote authors
 
 		try:
