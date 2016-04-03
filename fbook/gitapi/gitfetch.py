48c7798528d6cb1176e0ddf897a3333453c227e3
jiyange <program.ji.yang@gmail.com>
2016-04-03 13:22:56 -0600

    Fetch avatar url from github api

--- fbook/gitapi/gitfetch.py
+++ fbook/gitapi/gitfetch.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from urllib2 import urlopen, URLError, HTTPError # In python 3 change to urllib.*
+
+from urllib2 import urlopen, URLError, HTTPError
 import json
+import re
+
 
 def fetch_events(user_id):
     returned_list = []
     try:
         html = urlopen('https://api.github.com/users/' + user_id + '/events')
+        avatar = urlopen('https://api.github.com/users/' + user_id)
     # In python 3, add .decode('UTF-8') after read()
     except URLError as e:
         print(e.reason)
     else:
         json_str = (html.read())
         json_dict = json.loads(json_str)
+        ava_json_str = (avatar.read())
+        pattern = '"avatar_url":".*?"'
+        ava_url = re.findall(pattern, ava_json_str)[0][14:-1]
+        print ava_url
         for event in json_dict:
             result = {'id': event['id'],
                       'type': event['type'],
@@ -20,8 +28,14 @@ def fetch_events(user_id):
                       'repo': event['repo']['name'],
                       'repo_url': event['repo']['url'],
                       'public': event['public'],
-                      'created_at': event['created_at']}
-            print result
+                      'created_at': event['created_at'],
+                      'ava_url': ava_url
+                      }
+                      # 'avatar': ava_json_dict[0]['avatar_url']
+
             returned_list.append(result)
+    # print returned_list
     if len(returned_list) is not 0:
         return returned_list
+
+fetch_events('JiYangE')
