a23c3a64e0ca740f9a557024afe760a201a475d1
jiyange <program.ji.yang@gmail.com>
2016-04-03 16:13:26 -0600

    pep8

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from config import config as app_conf
 conf = os.getenv('FLASK_CONFIG') or 'default'
 app = create_app(app_conf[conf])
 
+
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Accept, Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  return response
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Accept, Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, threaded=True)
-
+    app.run(host='0.0.0.0', debug=True, threaded=True)
