1aa402491a5fd4adc8e466adb1db8879e530d62f
jiyange <program.ji.yang@gmail.com>
2016-04-03 20:02:57 -0600

    Type in views.py

--- fbook/main/views.py
+++ fbook/main/views.py
@@ -119,7 +119,7 @@ def index():
                            form=form,
                            name=current_user.username,
                            posts=posts,
-                           image={}
+                           image={},
                            github=github
                            )
 
