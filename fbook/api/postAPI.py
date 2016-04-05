6ae6c47fea36bb99e9db7d39906001285d2cc369
Aaron Tse <atse@uws092788usr.uws.ualberta.ca>
2016-04-04 19:01:05 -0600

    Fixed typo

--- fbook/api/postAPI.py
+++ fbook/api/postAPI.py
@@ -96,7 +96,7 @@ class BasePostAPI(Resource):
         query = Image_Posts.query.filter_by(post_id=cu.id).all()
         if len(query) > 0:
             image_id = query[0].__dict__['image_id']
-            post['iamge'] = 'http://floating-sands-69681.herokuapp.com/image/' + str(image_id)
+            post['image'] = 'http://floating-sands-69681.herokuapp.com/image/' + str(image_id)
         else:
             post['image'] = None
 
